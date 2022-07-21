d3b7e10afadcc428b0fafac1fbb2020eea2553ed
SuNameINA <suposoft@163.com>
2022-07-21 10:51:25 +0800

    commit 1

--- demo-fx-jfoenix2/src/main/java/com/example/fxjfoenix2/main/controller/CheckBoxController.java
+++ demo-fx-jfoenix2/src/main/java/com/example/fxjfoenix2/main/controller/CheckBoxController.java
@@ -2,7 +2,12 @@ package com.example.fxjfoenix2.main.controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class CheckBoxController {
 
@@ -18,20 +23,62 @@ public class CheckBoxController {
     @FXML
     private JFXButton button1;
 
+    Set<JFXCheckBox> selectedChekBox = new HashSet<>();
+
     public void initialize() throws Exception {
+        checkBox1.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (newValue.equals(Boolean.TRUE)) {
+                    selectedChekBox.add(checkBox1);
+                } else {
+                    selectedChekBox.remove(checkBox1);
+                }
+            }
+        });
+
+        checkBox2.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (newValue.equals(Boolean.TRUE)) {
+                    selectedChekBox.add(checkBox2);
+                } else {
+                    selectedChekBox.remove(checkBox2);
+                }
+            }
+        });
+
+        checkBox3.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (newValue.equals(Boolean.TRUE)) {
+                    selectedChekBox.add(checkBox3);
+                } else {
+                    selectedChekBox.remove(checkBox3);
+                }
+            }
+        });
         button1.setOnMouseClicked(e -> {
             StringBuilder builder = new StringBuilder();
-            if (checkBox1.isSelected()) {
-                builder.append("checkBox1 is selected").append(",");
+//            if (checkBox1.isSelected()) {
+//                builder.append("checkBox1 is selected").append(",");
+//            }
+//            if (checkBox2.isSelected()) {
+//                builder.append("checkBox2 is selected").append(",");
+//            }
+//            if (checkBox3.isSelected()) {
+//                builder.append("checkBox3 is selected").append(",");
+//            }
+//            String alertStr = builder.length() > 0 ? builder.substring(0, builder.length() - 1) : builder.toString();
+//            System.out.println(alertStr);
+
+            for (JFXCheckBox checkBox : selectedChekBox) {
+                if (builder.length() > 0) {
+                    builder.append(",");
+                }
+                builder.append(checkBox.getId());
             }
-            if (checkBox2.isSelected()) {
-                builder.append("checkBox2 is selected").append(",");
-            }
-            if (checkBox3.isSelected()) {
-                builder.append("checkBox3 is selected").append(",");
-            }
-            String alertStr = builder.length() > 0 ? builder.substring(0, builder.length() - 1) : builder.toString();
-            System.out.println(alertStr);
+            System.out.println(builder.toString());
         });
     }
 
