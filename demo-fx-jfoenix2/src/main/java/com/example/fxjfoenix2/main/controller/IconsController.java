b8a199c43818053906b602ce8b732b6597a69d9a
SuNameINA <suposoft@163.com>
2022-07-25 11:48:47 +0800

    commit 1

--- demo-fx-jfoenix2/src/main/java/com/example/fxjfoenix2/main/controller/IconsController.java
+++ demo-fx-jfoenix2/src/main/java/com/example/fxjfoenix2/main/controller/IconsController.java
@@ -1,10 +1,20 @@
 package com.example.fxjfoenix2.main.controller;
 
+import com.jfoenix.controls.JFXBadge;
 import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.controls.JFXSnackbar;
+import com.jfoenix.controls.JFXSnackbarLayout;
 import javafx.animation.Transition;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
 
 public class IconsController {
+
+    @FXML
+    private StackPane root;
 
     @FXML
     private JFXHamburger burger1;
@@ -18,12 +28,53 @@ public class IconsController {
     @FXML
     private JFXHamburger burger4;
 
+    @FXML
+    private JFXBadge badge1;
+
+    private int badgeClickCount = 1;
+
+    private JFXSnackbar snackbar;
+
     public void initialize() throws Exception {
         this.bindAction(burger1);
         this.bindAction(burger2);
         this.bindAction(burger3);
         this.bindAction(burger4);
 
+        snackbar = new JFXSnackbar(root);
+        snackbar.prefWidth(300);
+
+        badge1.setOnMouseClicked(event -> {
+//            badge1.setText(String.valueOf(++badgeClickCount));
+            int value = Integer.parseInt(badge1.getText());
+            if (event.getButton() == MouseButton.PRIMARY) {
+                value++;
+            } else if (event.getButton() == MouseButton.SECONDARY) {
+                value--;
+            }
+
+            if (value == 0) {
+                badge1.setEnabled(false);
+            } else {
+                badge1.setEnabled(true);
+            }
+
+            badge1.setText(String.valueOf(value));
+
+            if (badgeClickCount++ % 2 == 0) {
+                snackbar.fireEvent(new JFXSnackbar.SnackbarEvent(new JFXSnackbarLayout("Toast Message " + badgeClickCount)));
+            } else {
+                if (badgeClickCount++ % 4 == 0) {
+                    snackbar.fireEvent(new JFXSnackbar.SnackbarEvent(
+                            new JFXSnackbarLayout("Snackbar Message Persistent " + badgeClickCount, "CLOSE", action -> snackbar.close()),
+                            Duration.INDEFINITE, null));
+                } else {
+                    snackbar.fireEvent(new JFXSnackbar.SnackbarEvent(
+                            new JFXSnackbarLayout("Snackbar Message " + badgeClickCount, "UNDO", action -> snackbar.close()),
+                            Duration.millis(3000), null));
+                }
+            }
+        });
     }
 
     private void bindAction(JFXHamburger burger) {
