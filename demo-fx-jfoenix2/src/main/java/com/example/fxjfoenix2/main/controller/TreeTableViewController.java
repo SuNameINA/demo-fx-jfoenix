23553dd20a98c1a556d3b9571a6de4a34e0db742
SuNameINA <suposoft@163.com>
2022-07-27 11:51:11 +0800

    commit 1

--- demo-fx-jfoenix2/src/main/java/com/example/fxjfoenix2/main/controller/TreeTableViewController.java
+++ demo-fx-jfoenix2/src/main/java/com/example/fxjfoenix2/main/controller/TreeTableViewController.java
@@ -1,4 +1,17 @@
 package com.example.fxjfoenix2.main.controller;
 
 public class TreeTableViewController {
+
+    public void initialize() throws Exception {
+        this.setupReadOnlyTableView();
+        this.setupEditableTableView();
+    }
+
+    private void setupReadOnlyTableView() {
+
+    }
+
+    private void setupEditableTableView() {
+
+    }
 }
